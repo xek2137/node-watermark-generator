a40bf86a60d08bb63e1bbb199a4dd8a84e6fb4f0
Adrian Piech <adrianpiech.git@gmail.com>
2021-07-24 23:26:35 +0200

    Add try...catch functionality && refactor code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Jimp = require('jimp');
 const inquirer = require('inquirer');
+const {existsSync} = require('fs');
 
 const prepareOutputFilename = (filename) => {
   const [name, ext] = filename.split('.');
@@ -7,30 +8,42 @@ const prepareOutputFilename = (filename) => {
 };
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  const textData = {
-    text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    const textData = {
+      text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
 
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-  await image.quality(100).writeAsync(outputFile);
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+    await image.quality(100).writeAsync(outputFile);
+    console.log('Well done..');
+    startApp();
+  } catch (error) {
+    throw ('Something went wrong... Try again');
+  }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  await watermark.resize(300, 300);
-  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    await watermark.resize(300, 300);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, x, y, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.5,
-  });
-  await image.quality(100).writeAsync(outputFile);
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
+    await image.quality(100).writeAsync(outputFile);
+    console.log('Well done..');
+    startApp();
+  } catch (error) {
+    throw ('Something went wrong... Try again');
+  }
 };
 
 const startApp = async() => {
@@ -47,7 +60,7 @@ const startApp = async() => {
     name: 'inputImage',
     type: 'input',
     message: 'What file do you want to mark?',
-    default: 'adrian.jpg',
+    default: 'test.jpg',
   }, {
     name: 'watermarkType',
     type: 'list',
@@ -61,7 +74,12 @@ const startApp = async() => {
       message: 'Type your watermark text:',
     }]);
     options.watermarkText = text.value;
-    addTextWatermarkToImage(`./img/${options.inputImage}`, `./img/${prepareOutputFilename(options.inputImage)}`, `${options.watermarkText}`);
+
+    if (existsSync(`./img/${options.inputImage}`)) {
+      addTextWatermarkToImage(`./img/${options.inputImage}`, `./img/${prepareOutputFilename(options.inputImage)}`, `${options.watermarkText}`);
+    } else {
+      console.log('Something went wrong... Try again');
+    }
   } else {
     const image = await inquirer.prompt([{
       name: 'filename',
@@ -70,8 +88,13 @@ const startApp = async() => {
       default: 'logo.png',
     }]);
     options.watermarkImage = image.filename;
-    addImageWatermarkToImage(`./img/${options.inputImage}`, `./img/${prepareOutputFilename(options.inputImage)}`, `./img/${options.watermarkImage}`);
+
+    if (existsSync(`./img/${options.inputImage}`) && existsSync(`./img/${options.watermarkImage}`)) {
+      addImageWatermarkToImage(`./img/${options.inputImage}`, `./img/${prepareOutputFilename(options.inputImage)}`, `./img/${options.watermarkImage}`);
+    } else {
+      console.log('Something went wrong... Try again');
+    }
   }
-};
+}
 
 startApp();
